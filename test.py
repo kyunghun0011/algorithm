dccee9ef258edbcc71e2496b4fc1ed2a2ea86775
kyunghun0011 <kyunghun0011@github.com>
2021-02-21 21:04:30 +0900

    test

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
+
 print('hello world')
+
+a = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+print(a[4:])
+
+array = [i  for i in range(20) if i % 2 == 1]
+print(array)
+
+############
+n = 3
+m = 4
+array = [[0] * m for _ in range(n)] # N * M 크기의 2차원 리스트
+print(array)
